a3c784eec05e343c68fd7e8967acdd1a4ac16494
= <=>
2021-06-22 21:26:36 +0300

    init test task

---
+++ test_task/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+//    "fmt"
+    "net/http"
+    "encoding/json"
+//    "reflect"
+)
+
+
+func Respond(w http.ResponseWriter, httpStatus int, data map[string] interface{})  {
+	//w.WriteHeader(httpStatus)
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(httpStatus)
+	json.NewEncoder(w).Encode(data)
+}
+
+func main() {
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		mess := map[string]interface{}{
+			"status" : "false",
+		}
+		Respond(w, http.StatusNotFound, mess)
+	})
+
+	http.HandleFunc("/user/create", func(w http.ResponseWriter, r *http.Request) {
+		mess := map[string]interface{}{
+			"status" : "ok",
+			"message" : "user create",
+		}
+		Respond(w, http.StatusOK, mess)
+	})
+
+
+	http.HandleFunc("/user/login", func(w http.ResponseWriter, r *http.Request) {
+		mess := map[string]interface{}{
+			"status" : "ok",
+			"message" : "user login",
+		}
+		Respond(w, http.StatusOK, mess)
+	})
+
+
+	http.HandleFunc("/btcRate", func(w http.ResponseWriter, r *http.Request) {
+		mess := map[string]interface{}{
+			"status" : "fail",
+			"message" : 0.01,
+		}
+		Respond(w, http.StatusInternalServerError, mess)
+	})
+
+	http.ListenAndServe(":9990", nil)
+}
+
